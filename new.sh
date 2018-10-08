a14f7f2379ad7873048068ea3dafd601ce8c0696
abdelrahman_mohamed <abdomohamed00001@gmail.com>
2018-10-08 23:38:50 +0200

    add composer
    install composer and  insomnia tool to test api

--- new.sh
+++ new.sh
@@ -27,8 +27,23 @@ echo "#################################"
 echo "Install phpMyAdmin"
 echo "#################################"
 sudo apt-get install phpmyadmin -y
-
 echo "#################################"
 echo "Install composer php"
 echo "#################################"
-
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo apt-get install composer
+echo "#################################"
+echo "Install insomnia "
+echo "#################################"
+# Add to sources
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+# Add public key used to verify code signature
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+    | sudo apt-key add -
+# Refresh repository sources and install Insomnia
+sudo apt-get update
+sudo apt-get install insomnia
